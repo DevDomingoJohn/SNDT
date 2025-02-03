8be8b2960dbde582f05c9a2c14d1f87d51f12b08
DomingoJohnlery <domingojohnled@gmail.com>
2025-02-03 17:44:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/domin/sndt/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.domin.sndt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.domin.sndt.ui.theme.SNDTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SNDTTheme {
+
+            }
+        }
+    }
+}
